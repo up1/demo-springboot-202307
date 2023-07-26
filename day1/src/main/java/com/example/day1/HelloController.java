ef43db641f8dda14008657747b5e9f15513e06bb
up1 <somkiat.p@gmail.com>
2023-07-26 11:02:01 +0700

    Add autowired

--- day1/src/main/java/com/example/day1/HelloController.java
+++ day1/src/main/java/com/example/day1/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.day1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    HelloSpring helloSpring;
+
     @GetMapping("/hello")
     public HelloResponse sayHi(){
-        return new HelloResponse("Hello Spring");
+        String result = helloSpring.sayHi();
+        return new HelloResponse(result);
     }
 
 }

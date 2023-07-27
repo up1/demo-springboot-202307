528603f2467d476bc7f3044333f026dbd4099e9b
up1 <somkiat.p@gmail.com>
2023-07-27 11:14:38 +0700

    Integrate service and repository

--- day1/src/main/java/com/example/day1/users/UserCommandService.java
+++ day1/src/main/java/com/example/day1/users/UserCommandService.java
@@ -1,17 +1,27 @@
 package com.example.day1.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserCommandService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Integer createUser(CreateUserRequest newUser) {
-        // Case 1 :: Success
-        if(!"demo".equals(newUser.getFirst_name())) {
-            return 100;
+        // 1. Check firstname duplicate ?
+        List<MyTable> results = userRepository.findByFirstName(newUser.getFirst_name());
+        if(results != null && results.size() > 0) {
+            // Case 2 :: Firstname duplicated
+            throw new DuplicateFirstnameException("xxxx yyyy");
         }
-        // Case 2 :: Firstname duplicated
-        throw new DuplicateFirstnameException("xxxx yyyy");
+        // 2. Create new user
+        MyTable myTable = new MyTable(1, newUser.getFirst_name(), newUser.getLast_name());
+        userRepository.save(myTable);
+        return myTable.getId();
     }
 
 }
